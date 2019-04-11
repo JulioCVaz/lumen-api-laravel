140ab7166963935ba7ca8f6f1fd45dbabfe8e835
júlio vaz <julio.oliveiravaz0@gmail.com>
2019-04-11 00:46:21 -0300

    CRUD completo sem tratamento de erros

--- api-lumen/app/Http/Controllers/CarsController.php
+++ api-lumen/app/Http/Controllers/CarsController.php
@@ -3,33 +3,41 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Cars;
 
 class CarsController extends Controller
 {
+    private $model;
     /**
      * @return void
      */
-    public function __construct(){
-        // todo
+    public function __construct(Cars $cars){
+        $this->model = $cars;
     }
 
     public function getAll(){
-        return 'getAll';
+        $cars = $this->model->all();
+        return response()->json($cars);
     }
 
     public function get($id){
-        return "get " . $id;
+        $car = $this->model->find($id);
+        return response()->json($car);
     }
 
     public function store(Request $request){
-        dd($request->all());
+        $car = $this->model->create($request->all());
+        return response()->json($car);
     }
 
     public function update($id, Request $request){
-        dd($id, $request->id());
+        $car = $this->model->find($id)->update($request->all());
+        return response()->json($car);
     }
 
     public function destroy($id){
-        return $id;
+        $car = $this->model->find($id)->delete();
+
+        return response()->json(null);
     }
 }
